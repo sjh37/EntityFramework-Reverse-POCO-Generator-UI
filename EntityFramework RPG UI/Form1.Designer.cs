ea5476cd2439553c55ec258774a8f928a51fc497
Simon Hughes <simon@hicrest.net>
2014-11-05 23:19:57 +0000

    Anchor controls so they resize gracefully

--- EntityFramework RPG UI/Form1.Designer.cs
+++ EntityFramework RPG UI/Form1.Designer.cs
@@ -48,6 +48,9 @@
             // 
             // tables
             // 
+            this.tables.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tables.CheckOnClick = true;
             this.tables.FormattingEnabled = true;
             this.tables.Location = new System.Drawing.Point(13, 43);
