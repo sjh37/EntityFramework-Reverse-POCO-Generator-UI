cf7b5828424fb75f6dce3e920f151b055f04c9b3
ErikEJ <ejlskov@hotmail.com>
2017-02-03 14:44:32 +0100

    Added display of table schema

---
+++ EntityFramework RPG UI/TableSchema.cs
@@ -0,0 +1,23 @@
+﻿namespace EntityFramework_RPG_UI
+{
+    public class TableSchema
+    {
+        private readonly string _table;
+        private readonly string _schema;
+        public TableSchema(string table, string schema)
+        {
+            _table = table;
+            _schema = schema;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", _table, _schema);
+        }
+
+        public string TableName
+        {
+            get { return _table; }
+        }
+    }
+}
